1ad2e033cb306de52d4aad03b7c251abdcfa614d
Erik Onarheim <erik.onarheim@gmail.com>
2025-08-19 09:05:50 -0500

    up the timeout on playclick

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -2,18 +2,18 @@ import { test, expect } from '@playwright/test';
 
 test('main matches', async ({ page }) => {
   await page.goto('http://localhost:8080/sample.html');
-  await page.click('#excalibur-play');
+  await page.click('#excalibur-play', {timeout: 60_000});
   await expect(page).toHaveScreenshot();
 });
 
 test('autotile matches', async ({ page }) => {
   await page.goto('http://localhost:8080/autotile.html');
-  await page.click('#excalibur-play');
+  await page.click('#excalibur-play', {timeout: 60_000});
   await expect(page).toHaveScreenshot();
 });
 
 test('pure autotile matches', async ({ page }) => {
   await page.goto('http://localhost:8080/pureautotile.html');
-  await page.click('#excalibur-play');
+  await page.click('#excalibur-play', {timeout: 60_000});
   await expect(page).toHaveScreenshot();
 });
